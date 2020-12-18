6ef9d708aaee4a8e7e104688185550b758de40e8
Alex Antsiferov <wetterkrank@gmail.com>
2020-12-18 11:55:59 +0100

    added comments

--- aoc10_2.rb
+++ aoc10_2.rb
@@ -4,8 +4,8 @@ data = File.open('aoc10_input.txt').readlines.map { |x| x.chomp.to_i }
 data << 0
 data << data.max + 3
 
-groups = data.sort.chunk_while { |i, j| i + 1 == j }.map(&:size)
-hops = (3..groups.max).inject([0, 1, 1]) { |cmb, _| cmb << cmb[-3..-1].sum }
-paths = groups.map { |group| hops[group] }.reduce(:*)
+groups = data.sort.chunk_while { |i, j| i + 1 == j }.map(&:size) # split graph into sections separated by gaps
+combos = (3..groups.max).inject([0, 1, 1]) { |cmb, _| cmb << cmb[-3..-1].sum } # count combinations for each group size
+paths = groups.map { |group| combos[group] }.reduce(:*) # multiply to calculate the total
 
 p paths
