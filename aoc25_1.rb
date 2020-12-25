c942eb227926d4e688d22f45f3bad2d8a742b44b
Alex Antsiferov <wetterkrank@gmail.com>
2020-12-25 14:44:22 +0100

    it's nicer with reduce

--- aoc25_1.rb
+++ aoc25_1.rb
@@ -1,16 +1,15 @@
 # https://adventofcode.com/2020/day/25, Combo Breaker
 
-pubkeys = [14012298, 74241] # input
 # pubkeys = [5764801, 17807724] # test
+pubkeys = [14012298, 74241] # input
 
 def transform(subj, loop_size)
   loop_size.times.reduce(1) { |v, _| v * subj % 20201227 }
 end
 
-v = 1
-secret0 = 1.step do |i|
-  v = v * 7 % 20201227
+secret0 = (0..).reduce(1) do |v, i|
   break i if v == pubkeys[0]
+  v * 7 % 20201227
 end
 
 enc_key = transform(pubkeys[1], secret0)
