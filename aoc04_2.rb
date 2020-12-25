700c4697df15ea99bfea2193b1991bcc46f93ed0
Alex Antsiferov <wetterkrank@gmail.com>
2020-12-25 22:24:59 +0100

    draft

---
+++ aoc04_2.rb
@@ -0,0 +1,18 @@
+# https://adventofcode.com/2020/day/4
+
+REQUIRED = { 'byr' => '^(19[2-9][0-9]|200[0-2])$',
+             'iyr' => '^(201[0-9]|2020)$',
+             'eyr' => '^(202[0-9]|2030)$',
+             'hgt' => '^(1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in$',
+             'hcl' => '^#[0-9a-f]{6}$',
+             'ecl' => '^(amb|blu|brn|gry|grn|hzl|oth)$',
+             'pid' => '^\d{9}$' }
+
+data = File.open('aoc04_input.txt').read.split("\n\n")
+
+result = data.count do |passport|
+  fields = passport.split(/[\s\n]/).map { |f| f.split(':') }.to_h
+  REQUIRED.all? { |req, mask| fields.keys.include?(req) && fields[req].match?(mask) }
+end
+
+p result
