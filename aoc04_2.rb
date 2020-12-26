58dd172dedf30f96165bcb8a070be741b3d81e60
Alex Antsiferov <wetterkrank@gmail.com>
2020-12-26 14:47:32 +0100

    better name for rules

--- aoc04_2.rb
+++ aoc04_2.rb
@@ -1,6 +1,6 @@
 # https://adventofcode.com/2020/day/4
 
-REQUIRED = { 'byr' => '^(19[2-9][0-9]|200[0-2])$',
+VALID = { 'byr' => '^(19[2-9][0-9]|200[0-2])$',
              'iyr' => '^(201[0-9]|2020)$',
              'eyr' => '^(202[0-9]|2030)$',
              'hgt' => '^(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)$',
@@ -12,7 +12,7 @@ data = File.open('aoc04_input.txt').read.split("\n\n")
 
 result = data.count do |passport|
   fields = passport.split(/[\s\n]/).map { |f| f.split(':') }.to_h
-  REQUIRED.all? { |req, mask| fields.keys.include?(req) && fields[req].match?(mask) }
+  VALID.all? { |req, mask| fields.keys.include?(req) && fields[req].match?(mask) }
 end
 
 p result
